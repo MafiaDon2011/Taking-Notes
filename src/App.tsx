7dc9af27806a64db87642834de27aaae6e6a493e
MafiaDon2011 <wajidhussain1@live.com>
2023-02-02 11:37:34 +0000

    Added routing support for my note taking markdown support project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}/>
+      <Route path="/new" element={<h1>New</h1>}/>
+    </Routes>
+  )
+}
+
+export default App
