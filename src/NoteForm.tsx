4d9e6f5deacbec561967537e9926ebaf04baebdc
MafiaDon2011 <wajidhussain1@live.com>
2023-02-04 12:08:54 +0000

    created NewForm Page, added buttons to go back.

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,5 @@
-import { Form, Stack, Col, Row } from "react-bootstrap";
+import { Form, Stack, Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable"
 
 export function NoteForm() {
@@ -19,6 +20,16 @@ export function NoteForm() {
                         </Form.Group>
                     </Col>
                 </Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Body</Form.Label>
+                    <Form.Control required as="textarea" rows={15} />
+                </Form.Group>
+                <Stack direction="horizontal" gap={2} className="justify-content-end">
+                    <Button type="submit" variant="primary">Save</Button>
+                    <Link to="..">
+                    <Button type="button" variant="outline-secondary">Cancel</Button>
+                    </Link>
+                </Stack>
             </Stack>
         </Form>
     )
