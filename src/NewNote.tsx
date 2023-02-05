f3a1d4dcfb9dbb942be59b91a90eda5f9af305a1
MafiaDon2011 <wajidhussain1@live.com>
2023-02-05 13:48:42 +0000

    created local storage functions to store new tags

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,15 +1,17 @@
 import { NoteForm } from "./NoteForm";
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
 
 type NewNoteProps = {
     onSumbit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
 
-export function NewNote({onSumbit}: NewNoteProps) {
+export function NewNote({onSumbit, onAddTag, availableTags}: NewNoteProps) {
     return (
         <>
             <h1 className="mb-4">New Note</h1>
-            <NoteForm  onSubmit={onSumbit}/>
+            <NoteForm  onSubmit={onSumbit} onAddTag={onAddTag} availableTags={availableTags} />
         </>
     )
 }
