4c662fe5d3078291afb903f7ab6042dd2898f155
MafiaDon2011 <wajidhussain1@live.com>
2023-02-13 13:45:14 +0000

    fixed styling issues with cards

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -16,7 +16,7 @@ type NoteListProp = {
     notes: SimplifiedNote[]
 }
 
-export function NoteList({availableTags, notes }: NoteListProp) {
+export function NoteList({ availableTags, notes }: NoteListProp) {
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
     const [title, setTitle] = useState("")
 
@@ -75,18 +75,24 @@ export function NoteList({availableTags, notes }: NoteListProp) {
     </>
 }
 
-function NoteCard({id,title,tags}: SimplifiedNote) {
+function NoteCard({ id, title, tags }: SimplifiedNote) {
     return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
         <Card.Body>
-            <Stack gap={2} className="align-items-center justify-content-center h-100"></Stack>
-            <span className="fs-5">{title}</span>
-            {tags.length > 0 && (
-                <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
-                    {tags.map(tag => (
-                        <Badge className="text-truncate" key={tag.id}>{tag.label}</Badge>
-                    ))}
-                </Stack>
-            )}
+            <Stack gap={2} className="align-items-center justify-content-center h-100">
+                <span className="fs-5">{title}</span>
+                {tags.length > 0 && (
+                    <Stack
+                        gap={1}
+                        direction="horizontal"
+                        className="justify-content-center flex-wrap">
+                        {tags.map(tag => (
+                            <Badge className="text-truncate" key={tag.id}>
+                                {tag.label}
+                            </Badge>
+                        ))}
+                    </Stack>
+                )}
+            </Stack>
         </Card.Body>
     </Card>
 }
