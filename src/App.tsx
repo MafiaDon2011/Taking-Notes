b7cfde3e07efd2352c91a6c4ff971b58feb1cfd9
MafiaDon2011 <wajidhussain1@live.com>
2023-02-15 11:42:49 +0000

    added some code for naigations and redirections

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import useLocalStorage from "./useLocalStorage"
 import { useMemo } from "react"
 import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./NoteList"
+import { NoteLayout } from "./NoteLayout"
 
 export type Note = {
   id: string
@@ -57,7 +58,7 @@ function App() {
       <Routes>
         <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>}/>
         <Route path="/new" element={<NewNote onSumbit={onCreateNote} onAddTag={addTag} availableTags={tags}/>}/>
-        <Route path="/:id">
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" index element={<h1>Edit</h1>} />
         </Route>
